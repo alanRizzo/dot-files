21d773e11526bdd9d8d620e16803e5a5544a1823
Alan Rizzo <alan.rizzo@gmail.com>
2024-10-30 15:38:00 -0300

    DEAL WITH IT

--- nvim/lua/plugins/ui/catppucin.lua
+++ nvim/lua/plugins/ui/catppucin.lua
@@ -1,11 +1,9 @@
 -- Theme
 return {
-	{
-		"catppuccin/nvim",
-		name = "catppuccin",
-		priority = 1000,
-		config = function()
-			vim.cmd("colorscheme catppuccin-frappe")
-		end,
-	},
+	"catppuccin/nvim",
+	name = "catppuccin",
+	priority = 1000,
+	config = function()
+		vim.cmd("colorscheme catppuccin-frappe")
+	end,
 }
--- nvim/lua/plugins/ui/treesitter.lua
+++ nvim/lua/plugins/ui/treesitter.lua
@@ -1,33 +1,31 @@
 return {
-	{
-		"nvim-treesitter/nvim-treesitter",
-		event = { "BufReadPost", "BufNewFile" },
-		cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSModuleInfo" },
-		build = ":TSUpdate",
-		opts = {
-			indent = { enable = true },
-			ensure_installed = {
-				"bash",
-				"c",
-				"diff",
-				"html",
-				"json",
-				"lua",
-				"markdown",
-				"printf",
-				"python",
-				"query",
-				"toml",
-				"vim",
-				"yaml",
-			},
-			highlight = {
-				enable = true,
-				use_languagetree = true,
-			},
+	"nvim-treesitter/nvim-treesitter",
+	event = { "BufReadPost", "BufNewFile" },
+	cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSModuleInfo" },
+	build = ":TSUpdate",
+	opts = {
+		indent = { enable = true },
+		ensure_installed = {
+			"bash",
+			"c",
+			"diff",
+			"html",
+			"json",
+			"lua",
+			"markdown",
+			"printf",
+			"python",
+			"query",
+			"toml",
+			"vim",
+			"yaml",
 		},
-		config = function(_, opts)
-			require("nvim-treesitter.configs").setup(opts)
-		end,
+		highlight = {
+			enable = true,
+			use_languagetree = true,
+		},
 	},
+	config = function(_, opts)
+		require("nvim-treesitter.configs").setup(opts)
+	end,
 }
